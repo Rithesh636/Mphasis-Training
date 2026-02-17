261a9f299cfa1cd991e44209f8618478415a5fc7
Rithesh Poojary <rithesh.poojary@mphasis.com>
2026-02-17 16:51:31 +0530

    Custom tag example

---
+++ jsp_custom/src/main/java/com/test/MyTag.java
@@ -0,0 +1,29 @@
+package com.test;
+
+import java.util.Date;
+
+import jakarta.servlet.jsp.JspWriter;
+import jakarta.servlet.jsp.tagext.TagSupport;
+
+public class MyTag extends TagSupport {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public int doStartTag() 
+	{
+		JspWriter out = null;
+		
+		try {
+			out = pageContext.getOut();
+			
+			out.println("JSP custom tag : "+new Date());
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return SKIP_BODY;
+	}
+
+}
